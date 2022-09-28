dc3ff548d27eccece2e669f05e7ab2f9899b54d6
josephlarrivy <joseph.larrivy@gmail.com>
2022-09-28 14:19:50 -0500

    initial pages setup complete

--- app.py
+++ app.py
@@ -3,7 +3,20 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run
 
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = 'woeifwofb3454rf'
+debug = DebugToolbarExtension(app)
+
+
 @app.route('/')
-def homepage():
+def home():
     """return homepage"""
     return render_template('home.html')
+
+@app.route('/teams')
+def teams():
+    return render_template('teams.html')
+
+@app.route('/players')
+def homepage():
+    return render_template('players.html')
