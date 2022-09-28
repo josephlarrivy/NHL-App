9e6751f35f257b58b8bd44583d5f7aa5d600953d
josephlarrivy <joseph.larrivy@gmail.com>
2022-09-28 13:42:07 -0500

    second commit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 if __name__ == '__main__':
     app.run
 
 @app.route('/')
 def homepage():
-    
+    """return homepage"""
+    return render_template('home.html')
